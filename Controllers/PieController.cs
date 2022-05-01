4b0305628d34eec6bfb0673a15510d18d703dc24
Wael-Yassir <wael.yassir@gmail.com>
2022-05-01 16:46:41 +0200

    Add Pie controllers, view model and list view.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using BenthanysPieShop.Models;
+using BenthanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenthanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this._pieRepository = pieRepository;
+            this._categoryRepository = categoryRepository;
+        }
+
+        // The view that will be presented will havea the same name as the method by convensions
+        // the framework will search for a view under views folder with the same name as the controller
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel
+            {
+                Pies = _pieRepository.AllPies,
+                CurrentCategory = "Cheese Cake"
+            };
+
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ ViewModels/PiesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BenthanysPieShop.Models;
+using System.Collections.Generic;
+
+namespace BenthanysPieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
