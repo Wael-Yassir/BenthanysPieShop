a9ad97dd5ce634de059d54fcfbfa61290aa89d98
Wael-Yassir <wael.yassir@gmail.com>
2022-05-05 17:30:17 +0200

    Add routing features, and details view page.

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -27,5 +27,14 @@ namespace BenthanysPieShop.Controllers
 
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
