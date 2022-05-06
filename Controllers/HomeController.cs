e5c32ff69766f872ccd29d3dd9d9b407e526dd99
Wael-Yassir <wael.yassir@gmail.com>
2022-05-06 16:09:54 +0200

    Add Home controller, and view.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using BenthanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using BenthanysPieShop.ViewModels;
+
+namespace BenthanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this._pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BenthanysPieShop.Models;
+using System.Collections.Generic;
+
+namespace BenthanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
