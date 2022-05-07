daab757bead247b55a49a457acbb41810450965a
Wael-Yassir <wael.yassir@gmail.com>
2022-05-07 13:24:51 +0200

    Add CategoryMenu, and PieController custome conponents

---
+++ Components/ShoppingCartSummary.cs
@@ -0,0 +1,31 @@
+﻿using BenthanysPieShop.Models;
+using BenthanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenthanysPieShop.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            this._shoppingCart = shoppingCart;
+        }
+
+        // View components will search for a views on a subfolder called Components in the Shared folder.
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -1,6 +1,8 @@
 ﻿using BenthanysPieShop.Models;
 using BenthanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BenthanysPieShop.Controllers
 {
@@ -17,15 +19,38 @@ namespace BenthanysPieShop.Controllers
 
         // The view that will be presented will havea the same name as the method by convensions
         // the framework will search for a view under views folder with the same name as the controller
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel
-            {
-                Pies = _pieRepository.AllPies,
-                CurrentCategory = "Cheese Cake"
-            };
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel
+        //    {
+        //        Pies = _pieRepository.AllPies,
+        //        CurrentCategory = "Cheese Cake"
+        //    };
 
-            return View(piesListViewModel);
+        //    return View(piesListViewModel);
+        //}
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category).CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory,
+            });
         }
 
         public IActionResult Details(int id)
