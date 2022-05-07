6b05a07a09f9a91d1021c7901c7c3d850ee180b5
Wael-Yassir <wael.yassir@gmail.com>
2022-05-07 13:39:12 +0200

    Add email tag helpers.

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BenthanysPieShop.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mail-to:" + Address);
+            output.Content.SetContent(Content);
+        }
+    }
+}
