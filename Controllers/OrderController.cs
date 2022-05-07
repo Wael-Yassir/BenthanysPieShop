3e1ea9b7acff08787564605792ab05dec9bc1700
Wael-Yassir <wael.yassir@gmail.com>
2022-05-08 01:30:08 +0200

    Add checkout form.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using BenthanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenthanysPieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IOrderRepository _orderRepository { get; set; }
+        public ShoppingCart _shoppingCart { get; set; }
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this._orderRepository = orderRepository;
+            this._shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
