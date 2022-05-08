1e6126aad2af76175926de109f3731db7465b7ce
Wael-Yassir <wael.yassir@gmail.com>
2022-05-08 11:37:15 +0200

    Add order data validation.

--- Models/Order/Order.cs
+++ Models/Order/Order.cs
@@ -1,23 +1,67 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BenthanysPieShop.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
+
         public List<OrderDetail> OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your first name")]
+        [Display(Name = "Frist Name")]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your last name")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your address")]
+        [Display(Name = "Address Line 1")]
+        [StringLength(100)]
         public string AddressLine1 { get; set; }
+
+        [Display(Name = "Address Line 2")]
         public string AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your zip code")]
+        [Display(Name = "Zip Code")]
+        [StringLength(10, MinimumLength = 4)]
         public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your city")]
+        [StringLength(50)]
         public string City { get; set; }
+
+        [StringLength(10)]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your country")]
+        [StringLength(50)]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your phone number")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(25)]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your email")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
         public string Email { get; set; }
+
+        [BindNever]
+        [ScaffoldColumn(false)]
         public decimal OrderTotal { get; set; }
+
+        [BindNever]
         public DateTime OrderPlaced { get; set; }
     }
 }
